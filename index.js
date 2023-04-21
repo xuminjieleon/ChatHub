02dd619abdd4e91b9ce1a67b95e0426a21c6c595
leon.xu <leon.xu@ringcentral.com>
2023-04-21 22:03:22 +0800

    change port

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
 res.send('Hello World!')
 })
 
-app.listen(80, () => console.log(`微信公众号的 ${PORT} 端口服务启动成功^_^`));
+app.listen(PORT, () => console.log(`微信公众号的 ${PORT} 端口服务启动成功^_^`));
